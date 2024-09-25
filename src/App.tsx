2afeb6545a00554a2b76b2656a9db3d7c61a4cd9
AlanLvChen <alan.codemonkey@gmail.com>
2024-09-24 20:50:33 -0700

    added tailwind and analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './app/routes';
+import './styles/styles.css';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 
 const container = document.getElementById('root');
@@ -12,5 +15,7 @@ const root = createRoot(container!);
 root.render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
+    <SpeedInsights />
   </React.StrictMode>
 );
--- src/app/routes.tsx
+++ src/app/routes.tsx
@@ -20,6 +20,10 @@ const router = createBrowserRouter([
             },
         ]
     },
+    {
+        path: "*", 
+        element: <ErrorPage />,
+    },
 ]);
 
 
